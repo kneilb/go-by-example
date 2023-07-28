39fe9e2ba145d178eabb0ab1088496247e42f8e0
Neil Burningham <neil.burningham@mediakind.com>
2023-07-28 14:26:07 +0100

    Add text-templates (WIP)

---
+++ text-templates.go
@@ -0,0 +1,63 @@
+// https://gobyexample.com/text-templates
+//
+// text/template allows us to create dynamic content, or show custom output to the user.
+// there's also html/template, with the same API but extra security features for HTML.
+//
+// This is all rather familiar - because it's how Helm templates work.
+
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+func main() {
+
+	// TODO: comments from here!
+	t1 := template.New("t1")
+	t1, err := t1.Parse("Value is {{.}}\n")
+	if err != nil {
+		panic(err)
+	}
+
+	t1 = template.Must(t1.Parse("Value: {{.}}\n"))
+
+	t1.Execute(os.Stdout, "some text")
+	t1.Execute(os.Stdout, 5)
+	t1.Execute(os.Stdout, []string{
+		"Go",
+		"Rust",
+		"C++",
+		"C#",
+	})
+
+	Create := func(name, t string) *template.Template {
+		return template.Must(template.New(name).Parse(t))
+	}
+
+	t2 := Create("t2", "Name: {{.Name}}\n")
+
+	t2.Execute(os.Stdout, struct {
+		Name string
+	}{"Jane Doe"})
+
+	t2.Execute(os.Stdout, map[string]string{
+		"Name": "Mickey Mouse",
+	})
+
+	t3 := Create("t3",
+		"{{if . -}} yes {{else -}} no {{end}}\n")
+	t3.Execute(os.Stdout, "not empty")
+	t3.Execute(os.Stdout, "")
+
+	t4 := Create("t4",
+		"Range: {{range .}}{{.}} {{end}}\n")
+	t4.Execute(os.Stdout,
+		[]string{
+			"Go",
+			"Rust",
+			"C++",
+			"C#",
+		})
+}
