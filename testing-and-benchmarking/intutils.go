7f01a40eccc5e60610c74cb0fdbac46442a92233
Neil Burningham <neil.burningham@mediakind.com>
2023-08-23 09:44:55 +0100

    Add comments

--- testing-and-benchmarking/intutils.go
+++ testing-and-benchmarking/intutils.go
@@ -3,11 +3,13 @@
 // Unit testing is important, m'kay?
 // The testing package provides tools to write them, and "go test" runs them.
 //
-// This code is in the "main" package, but could be anywhere.
-// Test code typically lives in the same package as the code it tests.
+// I've split the tests & implementation into separate files, to show how it would work in the "real world".
+// I've also given this a more sane package name.
+// The tests are in intutils_test.go
 
 package intutils
 
+// We're going to test this function, which implements a simple integer minimum.
 func IntMin(a, b int) int {
 	if a < b {
 		return a
--- testing-and-benchmarking/intutils_test.go
+++ testing-and-benchmarking/intutils_test.go
@@ -3,8 +3,11 @@
 // Unit testing is important, m'kay?
 // The testing package provides tools to write them, and "go test" runs them.
 //
-// This code is in the "main" package, but could be anywhere.
-// Test code typically lives in the same package as the code it tests.
+// These are the tests.
+// The implementation we're testing is in intutils.go
+//
+// Run the tests using "go test -v"
+// Run the benchmarks using "go test -bench=."
 
 package intutils
 
@@ -13,6 +16,9 @@ import (
 	"testing"
 )
 
+// Create a test by writing a function whose name begins with Test.
+// t.Error* will report failures, but continue execution.
+// t.Fatal* will report failures and stop the test immediately.
 func TestIntMinBasic(t *testing.T) {
 	ans := IntMin(2, -2)
 	if ans != -2 {
@@ -20,6 +26,11 @@ func TestIntMinBasic(t *testing.T) {
 	}
 }
 
+// Writing tests can be repetitive, so we can use this table-driven style.
+// Inputs & expected outputs are listed in a table, and a loop walks over them.
+//
+// t.Run enables running "subtests", one per table entry.
+// These are shown separately when running with "go test -v"
 func TestIntMinTableDriven(t *testing.T) {
 	var tests = []struct {
 		a, b int
@@ -43,6 +54,10 @@ func TestIntMinTableDriven(t *testing.T) {
 	}
 }
 
+// Benchmark tests typically live in _test.go files, and are named starting with Benchmark.
+// The testing runner executes each function several times, increasing b.N until it gets a precise measurement.
+//
+// Typically the benchmark runs the function in a loop b.N times.
 func BenchmarkIntMin(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		IntMin(1, 2)
