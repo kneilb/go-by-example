193a035a311ea43dd071c42761b9bbe9231b700f
Neil Burningham <neil.burningham@mediakind.com>
2023-08-23 10:25:42 +0100

    Update comments

--- command-line-flags/command-line-flags.go
+++ command-line-flags/command-line-flags.go
@@ -3,6 +3,27 @@
 // Specify options for a program (e.g. wc -l)
 //
 // Go provides a flag package, which supports basic command-line parsing.
+//
+// Build it using "go build command-line-flags.go"
+//
+// Provide all values
+//  ./command-line-flags -word=opt -numb=7 -fork -svar=flag
+//
+// Omitted flags take their defaults
+//  ./command-line-flags -word=opt
+//
+// Trailing positional arguments can be provided after flags
+//  ./command-line-flags -word=opt a1 a2 a3
+//
+// The flag package requires all flags to appear before positional arguments!
+// Otherwise the flags are interpreted as position arguments...
+//  ./command-line-flags -word=opt a1 a2 a3 -numb=7
+//
+// Use --help or -h to get automatically generated help text.
+//  ./command-line-flags -h
+//
+// If you provide a flag that wasn't specified, you get an error message & the help text.
+//  ./command-line-flags --fishface
 
 package main
 
@@ -27,8 +48,10 @@ func main() {
 	var svar string
 	flag.StringVar(&svar, "svar", "bar", "a string var")
 
+	// Once it's all declared, run the parser.
 	flag.Parse()
 
+	// Print out the values we've parsed.
 	fmt.Println("word:", *wordPtr)
 	fmt.Println("num: ", *numPtr)
 	fmt.Println("fork:", *forkPtr)
